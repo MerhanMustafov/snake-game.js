138d5faf1dbf9c213a5196532d4bc23d62c42262
unknown <m.o.mustafov@gmail.com>
2022-07-09 23:29:48 +0300

    added boundary condition

--- game/snake.js
+++ game/snake.js
@@ -1,7 +1,7 @@
 /** @type {HTMLCanvasElement} */
 const board = document.querySelector("#board");
-board.width = window.innerWidth * 0.95;
-board.height = window.innerHeight * 0.8;
+board.width = window.innerWidth * 0.8;
+board.height = window.innerHeight * 0.75;
 
 const ctx = board.getContext("2d");
 
@@ -39,8 +39,6 @@ function gameOn() {
 		let goingRight = dx == HORIZONT_RIGT;
 		let goingUp = dy == VERTICAL_UP;
 		let goingDown = dy == VERTICAL_DOWN;
-		console.log("DX: " + Math.sign(dx));
-		console.log("DY: " + Math.sign(dy));
 		if (e.key == "ArrowRight" && (!goingLeft || dx == NEUTRAL)) {
 			moveSnake("X", "right");
 		}
@@ -49,11 +47,9 @@ function gameOn() {
 		}
 		if (e.key == "ArrowUp" && (!goingDown || dy == NEUTRAL)) {
 			moveSnake("Y", "up");
-			console.log("Pressed: " + e.key);
 		}
 		if (e.key == "ArrowDown" && (!goingUp || dy == NEUTRAL)) {
 			moveSnake("Y", "down");
-			console.log("Pressed: " + e.key);
 		}
 	});
 }
@@ -64,8 +60,30 @@ function drawSnake() {
 	snake.forEach((part) => {
 		ctx.beginPath();
 		ctx.rect(part.x, part.y, 10, 10);
+		ctx.fillStyle = "black"; //inside color
+		ctx.fill(); //inside color
+		ctx.strokeStyle = "red"; //border color
 		ctx.stroke();
 	});
+	hasGameEnded();
+}
+
+function hasGameEnded() {
+	let snakeHeadX = snake[0].x;
+	let leftWall = 0;
+	let rightWall = Math.trunc(board.width);
+	let snakeHeadY = snake[0].y;
+	let upWall = 0;
+	let downWall = Math.trunc(board.height);
+	if (snakeHeadX >= rightWall) {
+		alert("You hit the wall!");
+	} else if (snakeHeadX < leftWall) {
+		alert("You hit the wall!");
+	} else if (snakeHeadY >= downWall) {
+		alert("You hit the wall!");
+	} else if (snakeHeadY < upWall) {
+		alert("You hit the wall!");
+	}
 }
 
 function updateSnakePositionX(axis, direction) {
@@ -73,10 +91,6 @@ function updateSnakePositionX(axis, direction) {
 	snake.unshift(head);
 	snake.pop();
 	drawSnake();
-	// for (let i = 0; i < snake.length; i++) {
-	// 	snake[i].x += dx;
-	// 	drawSnake();
-	// }
 }
 function updateSnakePositionY(axis, direction) {
 	const head = { x: snake[0].x, y: snake[0].y + dy };
@@ -84,79 +98,46 @@ function updateSnakePositionY(axis, direction) {
 	snake.unshift(head);
 	snake.pop();
 	drawSnake();
-	// for (let i = 0; i < snake.length; i++) {
-	// 	snake[i].y += dy;
-	// 	drawSnake();
-	// }
 }
 
 function toRight(axis, direction) {
 	timeOut = setTimeout(() => {
 		updateSnakePositionX(axis, direction);
-		// drawSnake();
-		cancelAnimationFrame(reqId);
 		reqId = requestAnimationFrame(toRight);
 	}, interval);
 }
 function toLeft(axis, direction) {
 	timeOut = setTimeout(() => {
 		updateSnakePositionX(axis, direction);
-		// drawSnake();
-
-		cancelAnimationFrame(reqId);
 		reqId = requestAnimationFrame(toLeft);
 	}, interval);
 }
 function toDown(axis, direction) {
 	timeOut = setTimeout(() => {
 		updateSnakePositionY(axis, direction);
-		// drawSnake();
-		cancelAnimationFrame(reqId);
 		reqId = requestAnimationFrame(toDown);
 	}, interval);
 }
 function toUp(axis, direction) {
 	timeOut = setTimeout(() => {
 		updateSnakePositionY(axis, direction);
-		// drawSnake();
-		cancelAnimationFrame(reqId);
 		reqId = requestAnimationFrame(toUp);
 	}, interval);
 }
 
 function updateAxis(pressedButton) {
-	// let lastHorizontalAxisLeft = Math.sign(dx) == -1;
-	// let lastVerticalAxisUp = Math.sign(dy) == -1;
-	// let lastHorizontalAxisRight = Math.sign(dx) == 1;
-	// let lastVerticalAxisDown = Math.sign(dy) == 1;
-	// 	const NEUTRAL = 0
-
-	// const HORIZONT_LEFT_DIRECTION = -10;
-	// const HORIZONT_RIGT_DIRECTION = -10;
-
-	// const VERTICAL_UP_DIRECTION = -10
-	// const VERTICAL_DOWN_DIRECTION = 10
 	if (pressedButton == "right") {
-		// if (Math.sign(dx) == -1) {
 		dx = HORIZONT_RIGT;
-		dy = NEUTRAL; //transform from negative to positive number
-		// }
-	} else if (pressedButton == "left") {
-		// if (Math.sign(dx) == 1) {
-		dx = HORIZONT_LEFT; //transform from positive to negative number
 		dy = NEUTRAL;
-		// }
+	} else if (pressedButton == "left") {
+		dx = HORIZONT_LEFT;
+		dy = NEUTRAL;
 	} else if (pressedButton == "down") {
-		// if (Math.sign(dy) == -1) {
-		dy = VERTICAL_DOWN; //transform from negative to positive number
+		dy = VERTICAL_DOWN;
 		dx = NEUTRAL;
-		// }
 	} else if (pressedButton == "up") {
-		// if (Math.sign(dy) == 1) {
-		dy = VERTICAL_UP; //transform from positive to negative number
+		dy = VERTICAL_UP;
 		dx = NEUTRAL;
-
-		// }
 	}
 }
 function moveSnake(axis, direction) {
@@ -165,7 +146,6 @@ function moveSnake(axis, direction) {
 	updateAxis(direction);
 	if (direction == "right") {
 		toRight(axis, direction);
-		// toRight(axis, direction);
 	}
 	if (direction == "left") {
 		toLeft(axis, direction);
@@ -184,43 +164,4 @@ function clearBoard() {
 
 // COMMENT SECTION
 
-// let x = 0;
-// let y = 0;
-// for (let i = 0; i < 5; i++) {
-// 	ctx.beginPath();
-// 	ctx.rect(x, y, 20, 20);
-// 	ctx.stroke();
-// 	x += 20;
-// }
-
-// let head = { x: snake[i].x + dx, y: snake[i].y };
-// snake.shift();
-// snake.unshift(head);
-// snake.pop();
-
-// const boarder = {
-// 	right: snake[snake.length - 1].x == board.width * 0.97,
-// 	// left: snake[0].x == Math.trunc(board.width / 100),
-// 	left: snake[0].x < 2,
-// };
-// if (boarder.right) {
-// 	dx = -dx;
-// }
-// if (boarder.left) {
-// 	dx *= dx;
-// }
-
-// const snakeDirection = {
-// 	right: dx > 0 ? true : false,
-// 	left: dx < 0 ? true : false,
-// 	up: dy < 0 ? true : false,
-// 	down: dy > 0 ? true : false,
-// };
-
-// const pressed = {
-// 	right: [false, 0],
-// 	left: [false, 0],
-// 	up: [false, 0],
-// 	down: [false, 0],
-// };
 // COMMENT SECTION
